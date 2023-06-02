f52d9695d43ebf0b483d36e0c8fa23beaba94ce7
Odey John Ebinyi <odeyjhn@gmail.com>
2023-06-02 07:58:11 -0400

    online forum

--- oxford_brookes_forum/run.sh
+++ oxford_brookes_forum/run.sh
@@ -1,3 +1,3 @@
-# Igbe Chijioke
+# Odey John Ebinyi
 # This bash script compiles the index.cpp in this folder to index.cgi
 g++ -Wall -I/usr/include/cppconn -o index.cgi index.cpp ../is_user_loggedIn/check_online.cpp -lcrypto -L/usr/lib -lmysqlcppconn  -lcgicc
