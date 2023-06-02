df703cb0c39064367920abee380464649045eeef
Odey John Ebinyi <odeyjhn@gmail.com>
2023-06-02 08:14:33 -0400

    online forum

--- set_DB.sh
+++ set_DB.sh
@@ -1,3 +1,3 @@
-# Author: Igbe Chijioke
+# Author: Odey John Ebinyi
 # This bash script fills the mysql database with the setups in the file: create_database.sql
 mysql -u root -p < create_database.sql
